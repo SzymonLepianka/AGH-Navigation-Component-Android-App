4db8a285c1e01cfa07bf0c2ab5adaff7f42ff465
Szymon Lepianka <slepianka@wp.pl>
2023-01-21 22:51:51 +0100

    adds option menu (Settings and Terms fragments)

--- app/src/main/java/com/agh_navigation_component_android_app/MainActivity.kt
+++ app/src/main/java/com/agh_navigation_component_android_app/MainActivity.kt
@@ -2,11 +2,14 @@ package com.agh_navigation_component_android_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +27,21 @@ class MainActivity : AppCompatActivity() {
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.termsAndConditions) {
+            val action = NavGraphDirections.actionGlobalTermsFragment()
+            navController.navigate(action)
+            true
+        } else {
+            item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/agh_navigation_component_android_app/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.agh_navigation_component_android_app
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+}
