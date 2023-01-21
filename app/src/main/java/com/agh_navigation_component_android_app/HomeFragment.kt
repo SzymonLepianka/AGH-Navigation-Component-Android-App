625096b8d40d2ade382afc53d4429167a96ac2f3
Szymon Lepianka <slepianka@wp.pl>
2023-01-21 21:49:55 +0100

    navigating to login page

--- app/src/main/java/com/agh_navigation_component_android_app/HomeFragment.kt
+++ app/src/main/java/com/agh_navigation_component_android_app/HomeFragment.kt
@@ -1,6 +1,24 @@
 package com.agh_navigation_component_android_app
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val loginButton: Button = view.findViewById(R.id.button_login)
+        loginButton.setOnClickListener {
+
+            // this method name is generated from the ID of action in NavGraph
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+
+            // move to second fragment
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/com/agh_navigation_component_android_app/LoginFragment.kt
@@ -0,0 +1,6 @@
+package com.agh_navigation_component_android_app
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
